574f2ee1145d9d165346e815c9e3503e03a15d05
xuxinJS <xuxin.cnjs@gmail.com>
2021-06-12 10:29:41 +0800

    0612 update

---
+++ 2/2_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+
+    string carrots;
+    // carrots = cin.get();
+    cin >> carrots;
+
+    cout << "carrots:" << carrots << endl;
+    return 0;
+}
---
+++ 3/chartype.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+
+    cout << '$';
+    cout.put('$');
+    
+    return 0;
+}
